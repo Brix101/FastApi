6ffc123ef0cb8b593f0909fa1079ceba6d9f3609
brix <brixterporras@gmail.com>
2022-04-13 23:10:03 +0800

    Initial Commit

--- schema/todo_schema.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from pydantic import BaseModel
-
-class TodoSchema(BaseModel):
-    todo : str
-    complete : bool | None = False
-
-    class Config:
-        orm_mode = True
